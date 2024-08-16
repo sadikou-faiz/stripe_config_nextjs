cb433fc11ec01673a00afb4c9cfbc1d19def8481
faizdev <84401911+sadikou-faiz@users.noreply.github.com>
2024-08-16 11:40:44 +0200

    Update plans.ts

--- plans.ts
+++ plans.ts
@@ -1,13 +1,13 @@
 export const plans = [
     {
-        link: process.env.NODE_ENV === 'development' ? 'https://buy.stripe.com/test_5kA3cV4PS6vt6as28c' : '',
-        priceId: process.env.NODE_ENV === 'development' ? 'price_1PnLFx2KRuBKS7Wnkq0XdaeK' : '',
+        link: process.env.NODE_ENV === 'development' ? '' : '',
+        priceId: process.env.NODE_ENV === 'development' ? '' : '',
         price: 7,
         duration: '/month'
     },
     {
-        link: process.env.NODE_ENV === 'development' ? 'https://buy.stripe.com/test_3cs00Jbeg1b92YgeUZ' : '',
-        priceId: process.env.NODE_ENV === 'development' ? 'price_1PnLGh2KRuBKS7Wn1s3Oh4ag' : '',
+        link: process.env.NODE_ENV === 'development' ? '' : '',
+        priceId: process.env.NODE_ENV === 'development' ? '' : '',
 
         price: 70,
         duration: '/year'
